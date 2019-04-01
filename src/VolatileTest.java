10cb2fa42d46b752d463e7d83ed400066b344ace
Yu, Martin <dingxin.yu@uqconnect.edu.au>
2019-04-01 09:17:00 +0800

    add volatile test

---
+++ src/VolatileTest.java
@@ -0,0 +1,27 @@
+public class VolatileTest {
+    public volatile static int count = 0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    for (int j = 0; j < 100; j++)
+                        count++;
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count=" + count);
+    }
+
+}
